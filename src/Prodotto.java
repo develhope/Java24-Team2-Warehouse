6494fb8f8ec63d94cf618dad55ae72756d5e0dd7
Giovanni Zaffinelli <giovanni.zaffinelli99@gmail.com>
2024-03-09 12:26:39 +0100

    Inizializzazione progetto

---
+++ src/Prodotto.java
@@ -0,0 +1,12 @@
+public class Prodotto {
+    private TipoProdotto tipo;
+    private String produttore;
+    private String modello;
+    private String descrizione;
+    private float dimensioneDisplay;
+    private int spazioArchiviazione;
+    private float prezzoAcquisto;
+    private float prezzoVendita;
+    private int id;
+
+}
