f1635143f87b0c2f59bef920a005068e92189c2e
ValentinaFo <160612530+ValentinaFo@users.noreply.github.com>
2024-03-22 12:14:49 +0100

    implementati primi metodi del magazzino

--- src/Magazzino.java
+++ src/Magazzino.java
@@ -1,3 +1,43 @@
+import java.util.ArrayList;
+
 public class Magazzino {
+    private ArrayList<Prodotto> prodottiStored = new ArrayList<>();
+
+    public Magazzino() {
+    }
+
+    public ArrayList<Prodotto> getProdottiStored() {
+        return prodottiStored;
+    }
+
+    public void printProducts(){
+        for(Prodotto prodotto : prodottiStored){
+            System.out.println(prodotto);
+        }
+    }
+    public void searchTypeProduct(TipoProdotto x){
+        System.out.println("La lista del tipo di dispositivo ricercato è:");
+        for(Prodotto prodotto : prodottiStored){
+            if(prodotto.getTipo().equals(x)){
+                System.out.println(prodotto);
+            }
+        }
+    }
+    public void searchForProducer(String producer){
+        System.out.println("I dispositivi del produttore" + producer + "sono: ");
+        for(Prodotto prodotto : prodottiStored){
+            if(prodotto.getProduttore().equalsIgnoreCase(producer)){
+                System.out.println(prodotto);
+            }
+        }
+    }
+    public void searchForModel(String model){
+        System.out.println("I dispositivi per modello ricercato sono: ");
+        for(Prodotto prodotto : prodottiStored){
+            if(prodotto.getModello().equalsIgnoreCase(model)){
+                System.out.println(prodotto);
+            }
+        }
+    }
 
 }
